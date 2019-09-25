62d07e00cb93af0fb13a160217eafd57ea24a99d
aiv2008 <aiv2005@sina.com>
2019-09-25 17:47:40 +0800

    finish swap left and right node of the tree

--- practice/swap_l_r_node.h
+++ practice/swap_l_r_node.h
@@ -1,5 +1,8 @@
 #ifndef SWAP_L_R_NODE_H_INCLUDED
 #define SWAP_L_R_NODE_H_INCLUDED
+#define bool char
+#define true 1
+#define false 0
 
 typedef struct
 {
@@ -7,8 +10,14 @@ typedef struct
     size_t size;
 } tree, *p_tree, **p_p_tree;
 
-void swap_l_r_node(p_p_tree tree_param);
+void swap_l_r_node(p_p_tree pptree);
 
 void init_node(p_p_tree pptree, int* header, size_t size);
+
+void des_sort(p_p_tree pptree, size_t index_b, size_t index_e);
+
+bool is_nul(p_p_tree pptree);
+
+float my_log(int a, int b);
 
 #endif // SWAP_L_R_NODE_H_INCLUDED
