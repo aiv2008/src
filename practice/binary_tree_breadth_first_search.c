42b636d46fa9b8f3422f963d201c58d5bc2a31ea
aiv2008 <aiv2005@sina.com>
2019-10-19 00:05:03 -0400

    modify my heap function which do not use the memcpy function to copy the element, using the pointer instead

--- practice/binary_tree_breadth_first_search.c
+++ practice/binary_tree_breadth_first_search.c
@@ -20,27 +20,19 @@ void binaryTreeBreadthFirstTree(myBinrayTreeNode* pRoot)
     myBinrayTreeNode* root = '\0';
     myBinrayTreeNode* left ='\0';
     myBinrayTreeNode* right ='\0';
-    //while((*ppMyQueue)->bottom)
-    myQueueElem* pMove = queueTop(*ppMyQueue);
-    while(pMove != (*ppMyQueue)->bottom)
+    while((*ppMyQueue)->bottom)
+    //myQueueElem* pMove = queueTop(*ppMyQueue);
+    //while(pMove != (*ppMyQueue)->bottom)
     {
         top = queueTop(*ppMyQueue);
         root = (myBinrayTreeNode*)(top->data);
         printf("top=%d\n",root->data);
         left = root->left;
-printf("left passed\n");
         right = root->right;
-printf("right passed\n");
         if(left)queuePush(ppMyQueue, left, sizeof(myBinrayTreeNode));
-printf("left push passed\n");
         if(right)queuePush(ppMyQueue, right, sizeof(myBinrayTreeNode));
-printf("right push passed\n");
-        pMove = pMove->pNext;
-        //queuePop(*ppMyQueue);
-	//printf("pop passed\n");
+        //pMove = pMove->pNext;
+        queuePop(*ppMyQueue);
     }
-
-
     freeQueue(ppMyQueue);
-	return ;
 }
--- queue/my_queue_linkedlist.c
+++ queue/my_queue_linkedlist.c
@@ -25,54 +25,25 @@ void queuePush(myQueue** ppMyqueue, void* data, int elemLen)
         *ppMyqueue = (myQueue*)calloc(1, sizeof(myQueue));
     if(!(*ppMyqueue)->bottom)
     {
-	printf("enter p1\n");
-	printf("elemLen=%d\n",elemLen);
         (*ppMyqueue)->bottom = (myQueueElem*)calloc(1, sizeof(myQueueElem));
         myQueueElem* bottom = (*ppMyqueue)->bottom;
-        bottom->data = calloc(1, sizeof(elemLen));
-        memcpy(bottom->data, data, elemLen);
+	bottom->data = data;
+        //bottom->data = calloc(1, sizeof(elemLen));
+        //memcpy(bottom->data, data, elemLen);
         (*ppMyqueue)->top = (*ppMyqueue)->bottom;
     }
     else
     {
-printf("enter p2\n");
 	//new bottom
-myQueueElem* elem;
-printf("00000\n");
-printf("elemLen22=%d\n",elemLen);
-        if((elem = (myQueueElem*)calloc(1, sizeof(myQueueElem)))=='\0')
-	{
-		printf("malloc memory unsuccessful\n");
-		exit(1);
-	}
-	else
-	{
-		printf("calloc successfully\n");
-	}
-printf("aaa\n");
-        if((elem->data = calloc(1, sizeof(elemLen)))=='\0')
-	{
-		printf("malloc memory unsuccessful\n");
-		exit(1);
-	}
-	else
-	{
-		printf("calloc successfully\n");
-	}
-printf("bbb\n");
-        memcpy(elem->data, data, elemLen);
-printf("ccc\n");
-	//old bottom, new bottom-1
+	myQueueElem* elem = (myQueueElem*)calloc(1, sizeof(myQueueElem));
+	elem->data = data;
+	//old bottom
         myQueueElem* tmp = (*ppMyqueue)->bottom;
-printf("ddd\n");
         (*ppMyqueue)->bottom = elem;	
-printf("eee\n");
         tmp->pNext = elem;
-printf("fff\n");
 	elem = '\0';
 	tmp = '\0';;
     }
-	return ;
 }
 
 myQueueElem* queueTop(myQueue* pMyqueue)
@@ -110,7 +81,6 @@ void queuePop(myQueue* pMyqueue)
         free(top);
         top = '\0';
     }
-	return;
 }
 
 void freeQueue(myQueue** ppMyqueue)
@@ -120,49 +90,4 @@ void freeQueue(myQueue** ppMyqueue)
         free(*ppMyqueue);
         *ppMyqueue = '\0';
     }
-return ;
 }
-/**
-int main(void)
-{
-    srand(time(0));
-    int size = 10;
-    int a[size];
-    myQueue** ppMyqueue = (myQueue**)calloc(1, sizeof(myQueue*));
-    myQueueElem* bottom = '\0';
-    int i;
-    for(i=0;i<size;i++)
-    {
-        a[i] = rand()%size;
-        printf("%d,", a[i]);
-        queuePush(ppMyqueue, &a[i], sizeof(int));
-        bottom = queueBottom(*ppMyqueue);
-        printf("%d\n", *((int*)(bottom->data)));
-    }
-    printf("\n");
-//    int* top = (*ppMyqueue)->top;
-//    int* bottom = (*ppMyqueue)->bottom;
-//    printf("top=%d\n", *top);
-//    printf("bottom=%d\n", *bottom);
-
-    myQueueElem* top = '\0';
-//    top = (myQueueElem*)queueTop(*ppMyqueue);
-//    myQueueElem* pMove = top;
-//    while(pMove)
-//    {
-//        printf("%d,", *((int*)(pMove->data)));
-//        pMove = pMove->pNext;
-//    }
-
-
-    while((*ppMyqueue)->bottom)
-    {
-        top = (myQueueElem*)queueTop(*ppMyqueue);
-        printf("%d,", *((int*)(top->data)));
-        queuePop(*ppMyqueue);
-    }
-
-    freeQueue(ppMyqueue);
-    ppMyqueue = '\0';
-}
-**/
