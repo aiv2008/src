3af2dd9c42a55e1dd1839b8a81d0ebb776297c0d
aiv2008 <aiv2005@sina.com>
2019-11-05 03:02:19 -0500

    add permutation and permute

---
+++ permutation/permutation.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void permutation(char* pStr, char* pBegin)
+{
+	if(*pBegin == '\0')
+			printf("%s\n", pStr);
+	else
+	{
+		char* pCh;
+		for(pCh = pBegin; *pCh != '\0'; ++pCh)
+		{
+			printf("---befor---\n");
+			printf("*pCh=%s,*pBegin=%a\n", pCh, pBegin);
+			char temp = *pCh;
+			*pCh = *pBegin;
+			*pBegin = temp;
+			printf("*pCh=%s,*pBegin=%s\n", pCh, pBegin);
+			permutation(pStr, pBegin + 1);
+			printf("---after---\n");
+			printf("*pCh=%s,*pBegin=%s\n", pCh, pBegin);
+			temp = *pCh;
+			*pCh = *pBegin;
+			*pBegin = temp;
+			printf("*pCh=%s,*pBegin=%s\n", pCh, pBegin);
+		}
+	}		
+}
+
+
+
+int calN(int n)
+{
+	printf("n=%d,", n);
+	if(n > 0)
+	{
+		printf("aaa\n");
+		return n * calN(n-1);	
+	}
+	printf("bbb\n");
+	return 1;
+}
+
+int main(void)
+{
+	char pCh[] = "ABC";
+	permutation(pCh, pCh);
+	return 0;
+}
