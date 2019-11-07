9632fc23862128c9ed28ee10dfc748d10d7c5211
aiv2008 <aiv2005@sina.com>
2019-11-07 01:26:46 -0500

    permute finished, and testing passed

--- permute/permute.c
+++ permute/permute.c
@@ -3,18 +3,20 @@
 
 int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes)
 {
-	if(!nums || !numsSize)return NULL;
+	if(!nums || !numsSize || !returnSize)return NULL;
 	int size = calN(numsSize);
 	int** result = (int**)calloc(size, sizeof(int*));
-	//int** ppMove = (int**)calloc(size, sizeof(int*));
-	permutation(nums, nums, numsSize, result, size, &result);
-	returnSize = &size;
+	int** resultMove = result;
+	//permutation(nums, nums, numsSize, result, size, &result);
+	permutation(nums, nums, numsSize, size, &resultMove);
+	//printf("size=%d\n", size);
+	*returnSize = size;
 	return result;	
 }
 
-void permutation(int* pStr, int* pBegin, int size, int** result,int resultSize, int*** pppMove)
+void permutation(int* pStr, int* pBegin, int size, int resultSize, int*** pppMove)
 {
-	if(*pppMove - result > resultSize)return;
+	//if(*pppMove - result > resultSize)return;
 	if(pBegin - pStr == size)
 	{
 		if(!*pppMove)printf("*pppMove is null\n");
@@ -24,31 +26,32 @@ void permutation(int* pStr, int* pBegin, int size, int** result,int resultSize, 
 		int* p = pStr;
 		for(i=0;i<size;i++)	
 		{
-			*(pMove+i) = *(p+i);
-			printf("%d,",*(pMove+i));
+			//*(pMove+i) = *(p+i);
+			*(**pppMove+i) = *(p+i);
+			//printf("%d,",*(pMove+i));
+			//printf("%d,", *(**pppMove+i));
 		}
-		printf("\n");
+		//printf("\n");
 		(*pppMove)++;
 	}
 	else
 	{
 		int* pMove ;
-		int i;
-		for(pMove = pBegin; pMove - pStr < size; pMove++)
+		for(pMove = pBegin; pMove - pStr < size; ++pMove)
 		{
-			printf("---before---\n");
-			printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
+			//printf("---before---\n");
+			//printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
 			int temp = *pMove;
 			*pMove = *pBegin;
 			*pBegin = temp;
-			printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
-			permutation(pStr, pBegin+1, size, result,resultSize, pppMove);
-			printf("---after---\n");
-			printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
-			temp = *pBegin;
+			//printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
+			permutation(pStr, pBegin+1, size, resultSize, pppMove);
+			//printf("---after---\n");
+			//printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
+			temp = *pMove;
 			*pMove = *pBegin;
 			*pBegin = temp;
-			printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
+			//printf("*pMove=%d,*pBegin=%d\n", *pMove, *pBegin);
 		}
 	}				
 }
@@ -64,16 +67,28 @@ int calN(int n)
 
 int main(void)
 {
-	int nums[] = {1,2,3};
+	int nums[] = {2,1,2,3,7,21};
 	int numsSize = sizeof(nums)/sizeof(nums[0]);
-	int* size = NULL;
-	int** result = permute(nums, numsSize, size, NULL);
+	int size = 0;
+	
+	printf("aaa\n");
+	int** result = permute(nums, numsSize, &size, NULL);
+	printf("aaa\n");
+	if(!result)
+	{
+		printf("result is null\n");
+	}
+	else
+	{
+		if(!*result)printf("*result is null\n");
+	}
+	printf("%d\n", size);
 	int i;
 	for(i=0;i<size;i++)
 	{
 		int j;
 		for(j=0;j<numsSize;j++)
-			printf("", *(*(result+i)+j));
+			printf("%d,", *(*(result+i)+j));
 		printf("\n");
 	}
 	return 0;
